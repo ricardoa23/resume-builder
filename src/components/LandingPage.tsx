b9c77903e77d312ffffa26a857cf7211a48254c5
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-09-12 17:55:45 -0700

    finished landing page comp

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,10 +3,21 @@ import React from 'react'
 const LandingPage = () => {
   return (
     <>
-    <p>This Web Application allows users to create a create a resume from scratch and only input there job(s) descriptions and use chat GPT's api to improve the resume and return a filled out template in a .docx file. You'll be able to choose from a set of resume templates.</p>
+    <h1>Resume Builder</h1>
+    <p className='introduction'>This Web Application allows users to create a create a resume from scratch and only input there job(s) descriptions and use chat GPT's api to improve the resume and return a filled out template in a .docx file. You'll be able to choose from a set of resume templates.</p>
 
     <h2>How it Works</h2>
-    <p>If you're creating a new resume just fill out the form.</p>
+    <p className='how-it-works'>If you're creating a new resume just fill out the forms select a template you like and we'll do the rest. Your resume is written and optimized by AI. if you already have a resume you'd like to use just paste in you resume to the appropriate field and let us improve on your already great resume!</p>
+
+    <h2>Technologies Used </h2>
+    <p className='tech-used'>This single page web application was created using typescript and react using vite to initally set it up. the application uses <span><a href="">Open AI's API</a></span> </p>
+
+
+{/* button */}
+    <button>Get Started</button>
+
+    {/* Disclaimer needs to be set up */}
+
     </>
   )
 }
