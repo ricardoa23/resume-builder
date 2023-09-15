ab157ac50837231fcafdadf51999cd1fbdbdb755
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-09-15 12:47:59 -0700

    started working modal component

---
+++ src/components/AccessKeyModal.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AccessModal  = () => {
+  return (
+    <>
+        <div className="">
+            <h1>Request Access</h1>
+            <p>To experience the full application you'll need access. request access below</p>
+            
+        </div>
+    </>
+  )
+}
+
+export default AccessModal
