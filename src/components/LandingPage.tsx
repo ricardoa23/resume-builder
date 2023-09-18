588016f227afb60a5fec79538e483de0decc5de8
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-09-18 18:29:32 -0500

    improved landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,25 +1,74 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
   return (
     <>
-    <h1>Resume Builder</h1>
-    <p className='introduction'>This Web Application allows users to create a create a resume from scratch and only input there job(s) descriptions and use Open AI's API to improve the resume and return a filled out template in a .docx file. You'll be able to choose from a set of resume templates.</p>
+      <h1>Resume Builder</h1>
+      <p className="introduction">
+        Welcome to my single-page web application, designed to simplify and
+        elevate your resume creation process. I've crafted this platform to
+        empower you to create standout resumes effortlessly. Simply input your
+        personal information and work history, and our integration with OpenAI's
+        API takes care of the rest. The magic happens when I refine your work
+        history's bullet points, enhancing the quality of your resume. With just
+        a few clicks, you can generate a professionally polished resume in DOCX
+        format. Say goodbye to tedious formatting, and hello to your future
+        career success with my resume builder.{" "}
+      </p>
 
-    <h2>How it Works</h2>
-    <p className='how-it-works'>If you're creating a new resume just fill out the forms select a template you like and we'll do the rest. Your resume is written and optimized by AI. if you already have a resume you'd like to use just paste in you resume to the appropriate field and let us improve on your already great resume!</p>
+      <h2>How it Works</h2>
+      <p className="how-it-works">
+        Curious about how my platform transforms your work history into a
+        compelling resume? It's simple. After you input your details, the system
+        leverages OpenAI's API to analyze and improve the bullet points in your
+        work history. This means your resume not only looks polished but also
+        communicates your achievements and skills effectively. My platform
+        streamlines the entire process, ensuring that you get a high-quality
+        resume in no time. Experience the future of resume building with ease
+        and efficiency.
+      </p>
 
-    <h2>Technologies Used</h2>
-    <p className='tech-used'>This single page web application was created using typescript and react using vite to initally set it up. the application uses <span><a href="">Open AI's API</a></span> </p>
+      <h2>Technologies Used</h2>
+      <p className="tech-used">
+        My web application was meticulously crafted using cutting-edge
+        technologies to provide you with a seamless experience. The frontend is
+        built with React and TypeScript, offering a dynamic and responsive user
+        interface. On the backend, I rely on Node.js to handle data processing
+        and API interactions. Leveraging OpenAI's API, I enhance the quality of
+        your resume's content. To generate DOCX files, I use a powerful document
+        library. Initially, I employed Vite to accelerate the development
+        process, ensuring my application is fast and efficient. With this tech
+        stack, I deliver a user-friendly and high-performance resume builder to
+        help you stand out in your job search.
+      </p>
 
+      {/* button */}
+      <button>Get Started</button>
 
-{/* button */}
-    <button>Get Started</button>
+      <h2>Disclaimer</h2>
+      <p>
+        To unlock the full potential of my application, including the bullet
+        point improvement feature powered by OpenAI's API, I kindly request that
+        users submit an approval request. This process helps maintain the
+        integrity of my service and prevents misuse of API calls. However,
+        please note that even without approval, my application remains fully
+        functional, with the exception of the OpenAI-powered enhancements.
+      </p>
 
-    {/* Disclaimer needs to be set up */}
-
+      <h2>Privacy Policy</h2>
+      <p>
+        Rest assured, your privacy and data security are of utmost importance to
+        me. I want to reassure you that no personal information you provide will
+        be stored long-term or used for any purposes beyond the immediate scope
+        of generating your resume. I have designed my application with safety
+        and privacy in mind, implementing robust measures to protect your data
+        throughout your interaction with my platform. Thank you for choosing my
+        service. Your trust in me is greatly appreciated, and I am committed to
+        delivering a secure and efficient experience while prioritizing your
+        privacy at every step.it
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
