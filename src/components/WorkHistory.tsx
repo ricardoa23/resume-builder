b5718cb721aa6aadc3bedc9a3700a4e27996452d
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-10-11 17:12:40 -0500

    updated verification to be more functional updated it in education -- still working workHistory

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 import { formatDateToMonthYear as dateFormatter } from "../utilities/common";
 import { WorkHistory as WokrHistoryType } from "../utilities/workHistory";
 
@@ -8,13 +8,30 @@ import { WorkHistory as WokrHistoryType } from "../utilities/workHistory";
     * - create a button to remove work history
     * - create a button to add more work history (max of 4)
 */
-const WorkHistory = () => {
+
+const initialWorkHistory: WokrHistoryType = {
+  title: "",
+  company: "",
+  startDate: new Date(),
+  endDate: new Date(),
+  duties: [],
+};
+
+const WorkHistory: React.FC = () => {
   const [workHistory, setWorkHistory] = useState<WokrHistoryType[]>([]);
 
   const handleAddJob = () => {
-    //TODO: add a new job to the workHistory array
-    
+    setWorkHistory([
+      ...workHistory,
+      { ...initialWorkHistory, startDate: new Date(), endDate: new Date() },
+    ]);
   };
+
+  const handleRemoveJob = (index: number) => {
+    const updatedWorkHistory = [...workHistory];
+    updatedWorkHistory.splice(index, 1);
+    setWorkHistory(updatedWorkHistory);
+  }
 
   const handleInputChange = (
     index: number,
@@ -26,24 +43,72 @@ const WorkHistory = () => {
     setWorkHistory(updatedWorkHistory);
   };
 
+  const handleNext = () => {
+    localStorage.setItem("jobs", JSON.stringify(workHistory));
+  };
+
+
+
+  // const verifyJobObjectInLocalStorage = () => {
+  //   const jobs = localStorage.getItem("jobs");
+  //   if (!jobs) {
+  //   console.log("No jobs in local storage");
+  //   return false;
+  //   }
+
+  //   try {
+  //     const jobsArray = JSON.parse(jobs);
+
+  //     if(!Array.isArray(jobsArray) || jobsArray.length === 0) {
+  //       console.log("Jobs array is empty");
+  //       return false;
+  //     }
+      
+    
+  //   }
+  // }
+
+
   return (
     <>
       {workHistory.map((job, index) => (
         <div key={index}>
           {/* Other input fields */}{" "}
           <label htmlFor="positionTitle">Title of the Position</label>
-          <input type="text" name="positionTitle" value={job.title} onChange={(e) => handleInputChange(index, 'title', e.target)} />
+          <input
+            type="text"
+            name="positionTitle"
+            value={job.title}
+            onChange={(e) => handleInputChange(index, "title", e.target)}
+          />
           <label htmlFor="companyName"> Name of the Company</label>
-          <input type="text" name="companyName" value={job.company} onChange={(e) => handleInputChange(index, 'company', e.target)}  />
+          <input
+            type="text"
+            name="companyName"
+            value={job.company}
+            onChange={(e) => handleInputChange(index, "company", e.target)}
+          />
           <label htmlFor="startDate"> Start Date</label>
-          <input type="month" name="startDate" value={dateFormatter(job.startDate)} onChange={(e) => handleInputChange(index, 'startDate', e.target)}  />
+          <input
+            type="month"
+            name="startDate"
+            value={dateFormatter(job.startDate)}
+            onChange={(e) => handleInputChange(index, "startDate", e.target)}
+          />
           <label htmlFor="endDate"> End Date</label>
-          <input type="month" name="endDate" value={dateFormatter(job.endDate)} onChange={(e) => handleInputChange(index, 'endDate', e.target)}  />
+          <input
+            type="month"
+            name="endDate"
+            value={dateFormatter(job.endDate)}
+            onChange={(e) => handleInputChange(index, "endDate", e.target)}
+          />
           <h3>Are you still working here</h3>
           <input type="checkbox" />
+          <button type="button" onClick={() => handleRemoveJob(index)}>Remove Job</button>
         </div>
       ))}
       <button onClick={handleAddJob}>Add Job</button>
+      <button onClick={handleNext}>Next</button>
     </>
   );
 };
--- src/utilities/common.ts
+++ src/utilities/common.ts
@@ -1,9 +1,99 @@
 //date Formatter helper function
 export const formatDateToMonthYear = (date: Date | undefined) => {
-    if (!date) return '';
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // padStart ensures the month is two digits
-    return `${year}-${month}`;
-  };
+  if (!date) return "";
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  return `${year}-${month}`;
+};
 
-  
+export const verifyObjectInLocalStorage = (objectKey: string) => {
+  const localStorageObject = localStorage.getItem(objectKey);
+  if (!localStorageObject) {
+    console.log("Object not found in local storage");
+    return false;
+  }
+
+  try {
+    const localStorageObjectParsed = JSON.parse(localStorageObject);
+
+    if (Array.isArray(localStorageObjectParsed)) {
+      localStorageObjectParsed.forEach((item, index) => {
+        console.log(`Item ${index}:`);
+        const objectKeys = Object.keys(item);
+        objectKeys.forEach((key) => {
+          console.log(`key: ${key}`);
+          console.log(`${key}: ${item[key]}`);
+        });
+      });
+    } else {
+      const objectKeys = Object.keys(localStorageObjectParsed);
+      objectKeys.forEach((key) => {
+        console.log(`key: ${key}`);
+        console.log(`${key}: ${localStorageObjectParsed[key]}`);
+      });
+    }
+
+    return true;
+  } catch (error) {
+    console.log(`Not working: ${error}`);
+    return false;
+  }
+};
+
+//original verifiation function for education
+// const verifyEducationObjectInLocalStorage = () => {
+//   const educationJSON = localStorage.getItem("education");
+
+//   if (!educationJSON) {
+//     console.log("No education array found in local storage.");
+//     return false;
+//   }
+
+//   try {
+//     const educationArray = JSON.parse(educationJSON) as EducationInterface[];
+
+//     if (!Array.isArray(educationArray) || educationArray.length === 0) {
+//       console.log("Education data in local storage is not a non-empty array.");
+//       return false;
+//     }
+
+//     for (const education of educationArray) {
+//       if (!education) {
+//         console.log("One of the education objects in local storage is null or undefined.");
+//         return false;
+//       }
+
+//       if (!Object.values(DegreeTypes).includes(education.degreeType as DegreeTypes)) {
+//         console.log("Invalid degreeType in one of the education objects.");
+//         return false;
+//       }
+
+//       if (typeof education.major !== "string" || education.major.trim() === "") {
+//         console.log("Invalid major in one of the education objects.");
+//         return false;
+//       }
+
+//       if (typeof education.universityName !== "string" || education.universityName.trim() === "") {
+//         console.log("Invalid universityName in one of the education objects.");
+//         return false;
+//       }
+
+//       if (typeof education.gpa !== "number" || isNaN(education.gpa) || education.gpa < 0 || education.gpa > 4) {
+//         console.log("Invalid GPA in one of the education objects.");
+//         return false;
+//       }
+
+//       if (!education.graduationDate || typeof((new Date(education.graduationDate))) !== 'object' ) {
+//         console.log('Invalid graduationDate in one of the education objects.');
+//         return false;
+//       }
+//     }
+
+//     console.log('Valid education array found in local storage:', educationArray);
+//     console.log(Object.keys(educationArray[0]));
+//     return true;
+//   } catch (error) {
+//     console.error('Failed to parse education array from local storage:', error);
+//     return false;
+//   }
+// };
